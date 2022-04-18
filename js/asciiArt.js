f71420adeb5e7d1e6f61a13ea78bd326016f039d
Aditya Harvi <aharvi@ualberta.ca>
2022-04-18 16:10:14 -0600

    ascii art

---
+++ js/asciiArt.js
@@ -0,0 +1,87 @@
+let asciiArt = [
+    `
+    Yb        dP  dP"Yb  88""Yb 88  dP 888888 8888b.      dP"Yb  88b 88  
+     Yb  db  dP  dP   Yb 88__dP 88odP  88__    8I  Yb    dP   Yb 88Yb88  
+      YbdPYbdP   Yb   dP 88"Yb  88"Yb  88""    8I  dY    Yb   dP 88 Y88  
+       YP  YP     YbodP  88  Yb 88  Yb 888888 8888Y"      YbodP  88  Y8  
+    8b    d8 Yb  dP    8b    d8    db     dP""b8 88  88 88 88b 88 888888 
+    88b  d88  YbdP     88b  d88   dPYb   dP   '" 88  88 88 88Yb88 88__   
+    88YbdP88   8P      88YbdP88  dP__Yb  Yb      888888 88 88 Y88 88""   
+    88 YY 88  dP       88 YY 88 dP""""Yb  YboodP 88  88 88 88  Y8 888888 
+    `,
+    `
+    88 888888 .dP"Y8    88b 88  dP"Yb  888888       db       88""Yb 88   88  dP""b8     
+    88   88   'Ybo."    88Yb88 dP   Yb   88        dPYb      88__dP 88   88 dP   '"     
+    88   88   o.'Y8b    88 Y88 Yb   dP   88       dP__Yb     88""Yb Y8   8P Yb  "88     
+    88   88   8bodP'    88  Y8  YbodP    88      dP""""Yb    88oodP 'YbodP'  YboodP     
+    88 888888 .dP"Y8       db       888888 888888    db    888888 88   88 88""Yb 888888 
+    88   88   'Ybo."      dPYb      88__   88__     dPYb     88   88   88 88__dP 88__   
+    88   88   o.'Y8b     dP__Yb     88""   88""    dP__Yb    88   Y8   8P 88"Yb  88""   
+    88   88   8bodP'    dP""""Yb    88     888888 dP""""Yb   88   'YbodP' 88  Yb 888888 
+    `,
+    `
+    888888 Yb    dP 888888 88b 88    888888 88  88  dP"Yb  88   88  dP""b8 88  88    88 888888               
+    88__    Yb  dP  88__   88Yb88      88   88  88 dP   Yb 88   88 dP   '" 88  88    88   88                 
+    88""     YbdP   88""   88 Y88      88   888888 Yb   dP Y8   8P Yb  "88 888888    88   88                 
+    888888    YP    888888 88  Y8      88   88  88  YbodP  'YbodP'  YboodP 88  88    88   88                 
+    8888b.   dP"Yb  888888 .dP"Y8 88b 88 888888    Yb        dP  dP"Yb  88""Yb 88  dP                        
+     8I  Yb dP   Yb 88__   'Ybo." 88Yb88   88       Yb  db  dP  dP   Yb 88__dP 88odP                         
+     8I  dY Yb   dP 88""   o.'Y8b 88 Y88   88        YbdPYbdP   Yb   dP 88"Yb  88"Yb                         
+    8888Y"   YbodP  888888 8bodP' 88  Y8   88         YP  YP     YbodP  88  Yb 88  Yb                        
+    88  88  dP"Yb  Yb        dP    8888b.   dP"Yb  888888 .dP"Y8    88 888888    888888 888888 888888 88     
+    88  88 dP   Yb  Yb  db  dP      8I  Yb dP   Yb 88__   'Ybo."    88   88      88__   88__   88__   88     
+    888888 Yb   dP   YbdPYbdP       8I  dY Yb   dP 88""   o.'Y8b    88   88      88""   88""   88""   88  .o 
+    88  88  YbodP     YP  YP       8888Y"   YbodP  888888 8bodP'    88   88      88     888888 888888 88ood8 
+    `,
+    `
+    8b    d8 88   88 .dP"Y8 888888    88""Yb 888888    888888 88  88 888888 
+    88b  d88 88   88 'Ybo."   88      88__dP 88__        88   88  88 88__   
+    88YbdP88 Y8   8P o.'Y8b   88      88""Yb 88""        88   888888 88""   
+    88 YY 88 'YbodP' 8bodP'   88      88oodP 888888      88   88  88 888888 
+    88  88    db    88""Yb 8888b.  Yb        dP    db    88""Yb 888888      
+    88  88   dPYb   88__dP  8I  Yb  Yb  db  dP    dPYb   88__dP 88__        
+    888888  dP__Yb  88"Yb   8I  dY   YbdPYbdP    dP__Yb  88"Yb  88""        
+    88  88 dP""""Yb 88  Yb 8888Y"     YP  YP    dP""""Yb 88  Yb 888888      
+    `,
+    `
+    88      dP"Yb   dP"Yb  88  dP    88""Yb  dP"Yb  888888 88  88                     
+    88     dP   Yb dP   Yb 88odP     88__dP dP   Yb   88   88  88                     
+    88  .o Yb   dP Yb   dP 88"Yb     88""Yb Yb   dP   88   888888                     
+    88ood8  YbodP   YbodP  88  Yb    88oodP  YbodP    88   88  88                     
+    Yb        dP    db    Yb  dP .dP"Y8                                               
+     Yb  db  dP    dPYb    YbdP  'Ybo."                                               
+      YbdPYbdP    dP__Yb    8P   o.'Y8b                                               
+       YP  YP    dP""""Yb  dP    8bodP'                                               
+    88""Yb 888888 888888  dP"Yb  88""Yb 888888                                        
+    88__dP 88__   88__   dP   Yb 88__dP 88__                                          
+    88""Yb 88""   88""   Yb   dP 88"Yb  88""                                          
+    88oodP 888888 88      YbodP  88  Yb 888888                                        
+     dP""b8 88""Yb  dP"Yb  .dP"Y8 .dP"Y8 88 88b 88  dP""b8       db                   
+    dP   '" 88__dP dP   Yb 'Ybo." 'Ybo." 88 88Yb88 dP   '"      dPYb                  
+    Yb      88"Yb  Yb   dP o.'Y8b o.'Y8b 88 88 Y88 Yb  "88     dP__Yb                 
+     YboodP 88  Yb  YbodP  8bodP' 8bodP' 88 88  Y8  YboodP    dP""""Yb                
+      .d    Yb        dP    db    Yb  dP    .dP"Y8 888888 88""Yb 888888 888888 888888 
+    .d88     Yb  db  dP    dPYb    YbdP     'Ybo."   88   88__dP 88__   88__     88   
+      88      YbdPYbdP    dP__Yb    8P      o.'Y8b   88   88"Yb  88""   88""     88   
+      88       YP  YP    dP""""Yb  dP       8bodP'   88   88  Yb 888888 888888   88   
+    `,
+    `
+    Yb        dP 88  88 Yb  dP    8888b.   dP"Yb  888888 .dP"Y8   
+     Yb  db  dP  88  88  YbdP      8I  Yb dP   Yb 88__   'Ybo."   
+      YbdPYbdP   888888   8P       8I  dY Yb   dP 88""   o.'Y8b   
+       YP  YP    88  88  dP       8888Y"   YbodP  888888 8bodP'   
+    888888 88  88 88 .dP"Y8    Yb        dP  dP"Yb  88""Yb 88  dP 
+      88   88  88 88 'Ybo."     Yb  db  dP  dP   Yb 88__dP 88odP  
+      88   888888 88 o.'Y8b      YbdPYbdP   Yb   dP 88"Yb  88"Yb  
+      88   88  88 88 8bodP'       YP  YP     YbodP  88  Yb 88  Yb 
+    `
+]
+
+setTimeout(() => {
+    console.log("Some of my favourite quotes I've heard over the years...");
+    console.log(`%c ${getRandomQuote()}`, 'font-family: monospace');
+}, 2000);
+
+function getRandomQuote() {
+    return asciiArt[Math.floor(Math.random() * asciiArt.length)];
+}
